02d9433944384380a870a1f86a5145af09f767a4
Daniele Ceribelli <danieleceribelli@gmail.com>
2024-06-18 11:08:43 +0200

    Now can recive data from network in correct way

--- AUExtractor.py
+++ AUExtractor.py
@@ -1,19 +1,17 @@
 import struct
 import asyncio
-import sys
+from PIL import Image
 
 from feat import Detector
 import socket
-
-import AUExtractorGUI
 
 # SETUP
 # detector used to acquire faces, landmarks and AUs
 detector = Detector(face_model='faceboxes', landmark_model='mobilefacenet', au_model='xgb')
 
 # socket setup
-IP = '127.0.0.1'
-PORT = 8080
+IP = ''
+PORT = 0
 socket = socket.socket()
 
 # names of aus
@@ -24,22 +22,47 @@ AUsNames = [
 ]
 
 
-async def mainLoop(displayGui=False):
+async def mainLoop():
     while True:
-        ausArray = bytearray()  # array ready to be sent
-        # 8 -> timestamp
-        # 8294400 -> frame size
-        data = socket.recv(8 + 8294400)
+        # Raw data -> first 8 bytes timestamp and other 8294400 frame bytes
+        data = b''
+
+        # get all data from socket
+        for i in range(0, 8100):
+            cd = socket.recv(4096)
+            data += cd
+            print("curr pkt len ", len(cd), " TOT LEN ", len(data))
+
+        print(data[0:15])
+        print("lunghezza finale: ", len(data))
+
+        # Unpack recived data
         timestamp, frame = unpackData(data)
-        frame = frame[:, :, 0:3]  # from BGRA to BGR, remove opacity
+
+        # from np.array of byte to PIL Image
+        frame = generateImage(frame)
+
+        # from BGRA to BGR, remove opacity
+        frame = frame[:, :, ::-1]
+
+        # get aus (list of double)
         curr_aus = await detectAus(frame)
+
         if len(curr_aus[0]) > 0:
+            # normalize aus
             aus_list = NormalizeData(curr_aus[0][0].tolist())
-            # convert AUs into byte
-            for au in aus_list:
-                ausArray.extend(struct.pack('f', au))
-            socket.send(bytearray(timestamp) + ausArray)  # send to server
+
+            # convert AUs into
+            for au in range(len(aus_list)):
+                aus_list[au] = fromDoubleToFloat(aus_list[au])
+
+            socket.send(timestamp + b''.join(struct.pack('d', num) for num in aus_list))  # send to server
         await asyncio.sleep(0.01)
+
+
+# Convert array of byte in PIL image, readable from py feat
+def generateImage(frame):
+    return Image.frombytes('RGBA', (1920, 1080), frame, 'raw', 'BGRA')
 
 
 # Unpack received data, first 8 bytes are timestamp, other ones are frame
@@ -56,16 +79,17 @@ async def detectAus(frame):
     return detector.detect_aus(frame, detected_landmarks)
 
 
-# normalize data for openFACS (used for previous tests)
+# normalize data in scale 0 to 100
 def NormalizeData(data: list):
     normalized_data = [int(val * 1000) / 10 for val in data]
     return normalized_data
 
 
+# Convert Double (8 byte) into float (4 byte)
+def fromDoubleToFloat(val):
+    return struct.pack('>f', struct.unpack('>d', val)[0])
+
+
 if __name__ == "__main__":
-    displayGui = False
-    if len(sys.argv) >= 2:
-        displayGui = sys.argv[1]
     socket.connect((IP, PORT))
-    AUExtractorGUI.OpenGUI()
-    asyncio.run(mainLoop(displayGui))
+    asyncio.run(mainLoop())
